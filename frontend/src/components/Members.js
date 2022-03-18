e031462d2fc0dcfdde9512c639cd6dc7cf86d720
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-03-18 10:29:36 -0400

    Pete Lofstedt

--- frontend/src/components/Members.js
+++ frontend/src/components/Members.js
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import styled from "styled-components";
 
 import { FaceContext } from "./FaceContext";
+import Profile from "./Profile";
 
 const Members = () => {
   const { members, setMembers } = useContext(FaceContext);
@@ -11,18 +12,36 @@ const Members = () => {
       try {
         const response = await fetch("/api/users/", {});
         const data = await response.json();
-        setMembers([data.data]);
+        setMembers(data.data);
       } catch (err) {
         console.log(err);
       }
     })();
   }, [setMembers]);
 
-  console.log(members);
-
-  return <Wrapper>All Facespace members</Wrapper>;
+  return (
+    <Wrapper>
+      <Heading>All Facespace members</Heading>
+      <PicGrid>
+        {members.length > 1 &&
+          members.map((el) => {
+            console.log(el);
+            return <ProfilePic key={el.id} src={el.avatarUrl} />;
+          })}
+      </PicGrid>
+    </Wrapper>
+  );
 };
 
 export default Members;
 
 const Wrapper = styled.h2``;
+const Heading = styled.h3``;
+const PicGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+`;
+const ProfilePic = styled.img`
+  width: 125px;
+`;
