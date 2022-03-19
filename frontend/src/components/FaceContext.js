e6c075862a5e61e90b812734d98a393933cfb0d8
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-03-19 00:15:05 -0400

    Pete Lofstedt

--- frontend/src/components/FaceContext.js
+++ frontend/src/components/FaceContext.js
@@ -4,7 +4,6 @@ export const FaceContext = createContext(null);
 export const FaceProvider = ({ children }) => {
   const [members, setMembers] = useState({});
   const [currentProfile, setCurrentProfile] = useState([]);
-  // const [currentProfileFriends, setCurrentProfileFriends] = useState([]);
 
   const loadMembers = async () => {
     try {
